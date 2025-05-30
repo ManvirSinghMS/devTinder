78e91e75c01281b6c30d6edd59744485c2378aed
Manvir Singh <Manvirs1611@gmail.com>
2025-05-30 12:40:37 +0530

    exploring get post apis

--- src/app.js
+++ src/app.js
@@ -5,6 +5,19 @@ const app  = express()
 app.get("/",(req, res)=>{
     res.send("not")
 })
+
+app.get("/user",(req,res)=>{
+    res.send({firstName: "Manvir", lastName: "Singh"})
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("User Deleted successfully")
+})
+
+app.post("/user",(req,res)=>{
+    res.send("We Post a new photo on instagram")
+})
+
 app.get("/test",(req, res)=>{
     res.send("Hiiiiiiii Guys Amerrikkkaa yaaa from express server")
 })
