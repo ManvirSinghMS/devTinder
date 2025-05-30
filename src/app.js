2e79200ecf25f5b3a5973556cf9d8666b194b1e1
Manvir Singh <Manvirs1611@gmail.com>
2025-05-30 20:21:00 +0530

    multiple middlewares

--- src/app.js
+++ src/app.js
@@ -2,44 +2,58 @@ const express = require("express")
 
 const app  = express()
 
-app.get("/",(req, res)=>{
-    res.send("not")
-})
+// app.get("/",(req, res)=>{
+//     res.send("not")
+// })
 
-app.get("/abcd", (req, res) => {
-    console.log(req.query)
-    res.send({ firstName: "Parnit", lastName: "Kaur" });
-});
-
-
-app.get("/abcd/:abcdId/:name/:password", (req, res) => {
-    console.log(req.params)
-    res.send({ firstName: "Parnit", lastName: "Kaur" });
-});
-
-app.get("/ac", (req, res) => {
-    res.send({ firstName: "Parnit", lastName: "Kaur" });
-});
+// app.get("/abcd", (req, res) => {
+//     console.log(req.query)
+//     res.send({ firstName: "Parnit", lastName: "Kaur" });
+// });
 
 
-app.get("/user",(req,res)=>{
-    res.send({firstName: "Manvir", lastName: "Singh"})
-})
+// app.get("/abcd/:abcdId/:name/:password", (req, res) => {
+//     console.log(req.params)
+//     res.send({ firstName: "Parnit", lastName: "Kaur" });
+// });
 
-app.delete("/user",(req,res)=>{
-    res.send("User Deleted successfully")
-})
+// app.get("/ac", (req, res) => {
+//     res.send({ firstName: "Parnit", lastName: "Kaur" });
+// });
 
-app.post("/user",(req,res)=>{
-    res.send("We Post a new photo on instagram")
-})
 
-app.get("/test",(req, res)=>{
-    res.send("Hiiiiiiii Guys Amerrikkkaa yaaa from express server")
-})
-app.get("/Hehehehe",(req, res)=>{
-    res.send("Muuuuhehehehhehhehhehhehehhehheehehehehhee")
-})
+// app.get("/user",(req,res)=>{
+//     res.send({firstName: "Manvir", lastName: "Singh"})
+// })
+
+// app.delete("/user",(req,res)=>{
+//     res.send("User Deleted successfully")
+// })
+
+// app.post("/user",(req,res)=>{
+//     res.send("We Post a new photo on instagram")
+// })
+
+// app.get("/test",(req, res)=>{
+//     res.send("Hiiiiiiii Guys Amerrikkkaa yaaa from express server")
+// })
+// app.get("/Hehehehe",(req, res)=>{
+//     res.send("Muuuuhehehehhehhehhehhehehhehheehehehehhee")
+// })
+
+app.use("/user", 
+    (req,res,next) =>{
+         console.log("2nd Response is running")
+          next()
+       res.send("first Response")
+      
+    },
+    (req,res)=>{
+        console.log("2nd Response is running")
+      res.send("second Response")
+    }
+)
+
 
 app.listen(2103, ()=>{
     console.log("Server is successfully running on port 2103....")
