3603d71d5f98ff09550fbc635a64a82145fb6bb6
Manvir Singh <Manvirs1611@gmail.com>
2025-05-30 22:28:52 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -1,29 +1,58 @@
 const express = require("express")
 
 const app  = express()
-const {adminAuth, userAuth} = require("./middlewares/auth")
-app.use("/admin",adminAuth)
 
-
-
-app.get("/user/userLogin", (req,res)=>{
-    res.send("User logged in successfully")
-})
-
-
-app.get("/user",userAuth, (req,res)=>{
-    res.send("User data Send")
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("Something went wrong")
+    }
 })
 
 
 
-app.get("/admin/getAllData", (req,res)=>{
-    res.send("All data Send")
+app.get("/getUserDetails",(req,res,next)=>{
+    // try{
+     throw new Error("ijfodj")
+     res.send("User Data Send")
+    // }catch(err){
+    //   res.status(500).send("some error has occured")
+    // }
 })
 
-app.get("/admin/deleteUser",(req,res)=>{
-    res.send("Deleted a User")
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("Something went wrong")
+    }
 })
+
+
+
+
+
+
+// const {adminAuth, userAuth} = require("./middlewares/auth")
+// app.use("/admin",adminAuth)
+
+
+
+// app.get("/user/userLogin", (req,res)=>{
+//     res.send("User logged in successfully")
+// })
+
+
+// app.get("/user",userAuth, (req,res)=>{
+//     res.send("User data Send")
+// })
+
+
+
+// app.get("/admin/getAllData", (req,res)=>{
+//     res.send("All data Send")
+// })
+
+// app.get("/admin/deleteUser",(req,res)=>{
+//     res.send("Deleted a User")
+// })
 
 
 app.listen(2103, ()=>{
