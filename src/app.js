470a0cdc34ce6517ebdbfff1c278bd7370c4b155
Manvir Singh <Manvirs1611@gmail.com>
2025-05-30 14:14:09 +0530

    params and queries

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ app.get("/",(req, res)=>{
     res.send("not")
 })
 
+app.get("/abcd", (req, res) => {
+    console.log(req.query)
+    res.send({ firstName: "Parnit", lastName: "Kaur" });
+});
+
+
+app.get("/abcd/:abcdId/:name/:password", (req, res) => {
+    console.log(req.params)
+    res.send({ firstName: "Parnit", lastName: "Kaur" });
+});
+
+app.get("/ac", (req, res) => {
+    res.send({ firstName: "Parnit", lastName: "Kaur" });
+});
+
+
 app.get("/user",(req,res)=>{
     res.send({firstName: "Manvir", lastName: "Singh"})
 })
