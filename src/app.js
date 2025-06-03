4b0542c4767306970686d244cc201fb48c97266a
Manvir Singh <Manvirs1611@gmail.com>
2025-06-03 23:06:47 +0530

    API LEVEL Data Sanitization

--- src/app.js
+++ src/app.js
@@ -1,79 +1,97 @@
-const express = require("express")
-const connectDB   = require("./config/database")
-const app  = express()
-const User = require("./models/user")
+const express = require("express");
+const connectDB = require("./config/database");
+const app = express();
+const User = require("./models/user");
 
-app.use(express.json())
+app.use(express.json());
 
-app.post("/signup",async (req,res)=>{
-   const user = new User(req.body)
-   try{
-  await user.save();
-  res.send("User Added Successfully")
-   } catch(err){
-    res.status(400).send("Error Saving the User : " + err.message)
-   }
-
-})
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("User Added Successfully");
+  } catch (err) {
+    res.status(400).send("Error Saving the User : " + err.message);
+  }
+});
 
 //API To get all the users
-app.get("/feed",async (req,res)=>{
-    try{
-    const users = await User.find({})
-    res.send(users)
-    }catch(err){
-     res.status(400).send("Something went wrong")
-    }
-})
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
 //API To get user by email
-app.get("/user",async(req,res)=>{
-    const userEmail = req.body.emailId
-    try{
-      const user = await User.find({emailId : userEmail})
-      if(!user){
-        res.status(400).send("User not found")
-      }else{
-      res.send(user)
-      }
-
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.find({ emailId: userEmail });
+    if (!user) {
+      res.status(400).send("User not found");
+    } else {
+      res.send(user);
     }
-    catch(err){
-        res.status(400).send("Unable to get the user")
-    }
-})
+  } catch (err) {
+    res.status(400).send("Unable to get the user");
+  }
+});
 
 //API To delete the user
-app.delete("/user",async(req,res)=>{
-    const UserId = req.body.UserId
-    try{
-        const user = await User.findByIdAndDelete({_id : UserId})
-        res.send("User Deleted Succcessfully")
-    } catch(err){
-        res.status(400).send("Unable to delete the user")
-    }
-})
+app.delete("/user", async (req, res) => {
+  const UserId = req.body.UserId;
+  try {
+    const user = await User.findByIdAndDelete({ _id: UserId });
+    res.send("User Deleted Succcessfully");
+  } catch (err) {
+    res.status(400).send("Unable to delete the user");
+  }
+});
 
 //API To Update the user
-app.patch("/user",async(req,res)=>{
-    const userId = req.body.userId
-    const data = req.body
-    try{
-        await User.findByIdAndUpdate({_id: userId},data)
-        res.send("User Updated")
-    }catch(err){
-        res.status(400).send("Something went wrong")
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+  try {
+    const ALLOWED_UPDATED = [
+      "password",
+      "about",
+      "photoUrl",
+      "gender",
+      "skills",
+    ];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATED.includes(k)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
     }
-})
+    if (data.skills && data?.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
+    }
+
+    const user = await User.findByIdAndUpdate({ _id: userId }, data,{
+        returnDocument: "after",
+        runValidators : true,
+    });
+    console.log(user)
+    res.send("User Updated");
+  } catch (err) {
+    res.status(400).send("UPDATE FAILED" + err.message);
+  }
+});
 
 connectDB()
-.then(()=>{
-    console.log("Database Connection is made")
-    app.listen(2103, ()=>{
-    console.log("Server is successfully running on port 2103....")
-})
-})
+  .then(() => {
+    console.log("Database Connection is made");
+    app.listen(2103, () => {
+      console.log("Server is successfully running on port 2103....");
+    });
+  })
 
-.catch((err)=>{
-    console.log("Database Connection Failed")
-})
+  .catch((err) => {
+    console.log("Database Connection Failed");
+  });
